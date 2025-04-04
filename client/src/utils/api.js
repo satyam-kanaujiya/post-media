3e998ef590c2eba4c0a4d92874c64fd7f01eb65c
satyam kanaujiya <satyamkanaujiya02@gmail.com>
2025-04-04 13:15:59 +0530

    add backend url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"http://127.0.0.1:8080/api/v1"
+    baseURL:"https://post-media-backend.vercel.app/api/v1"
 });
 
 export default api;
